8cf90417c12b84f2bb3dd4fd855973a7c0b4646d
Alejandro Martínez <imavca@gmail.com>
2021-06-13 15:33:13 -0500

    Initial Structure

---
+++ PopMovies/PopMovies/PopMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopMoviesApp.swift
+//  PopMovies
+//
+//  Created by Carlos Martínez on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PopMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
