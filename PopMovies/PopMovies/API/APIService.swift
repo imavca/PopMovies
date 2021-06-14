5e3c2b7f571c31922b2673919ceafba75eb38248
Alejandro Martínez <imavca@gmail.com>
2021-06-13 23:30:37 -0500

    Creates a Movie View to show dummy data

--- PopMovies/PopMovies/API/APIService.swift
+++ PopMovies/PopMovies/API/APIService.swift
@@ -42,6 +42,19 @@ extension Endpoint: APIBuilder {
     }
 }
 
+extension Endpoint {
+    enum ImageSize: String {
+        case small = "https://image.tmdb.org/t/p/w154/"
+        case medium = "https://image.tmdb.org/t/p/w500/"
+        case cast = "https://image.tmdb.org/t/p/w185/"
+        case original = "https://image.tmdb.org/t/p/original/"
+        
+        func path(poster: String) -> URL {
+            return URL(string: rawValue)!.appendingPathComponent(poster)
+        }
+    }
+}
+
 // MARK: - API Service
 
 struct MovieService: APIService {
